5e7c25dae6a91b32b25a089a17e8f803d9fc652d
LostSoul <kpimayakov@gmail.com>
2015-11-30 23:43:06 +0200

    added swagger

--- src/main/java/com/vitalsport/registration/RegistrationApplication.java
+++ src/main/java/com/vitalsport/registration/RegistrationApplication.java
@@ -4,8 +4,9 @@ import com.vitalsport.registration.config.PersistenceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+@EnableSwagger2
 @SpringBootApplication(exclude={PersistenceConfiguration.class})
 public class RegistrationApplication {
     public static void main(String[] args) {
