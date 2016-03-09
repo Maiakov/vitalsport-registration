48366df34531bf0158c8b10de8eaabb99887b7af
Rus1k <rfrasulov@gmail.com>
2016-03-10 01:55:11 +0200

    new branch

--- src/test/java/com/vitalsport/registration/service/ValidateEmailTest.java
+++ src/test/java/com/vitalsport/registration/service/ValidateEmailTest.java
@@ -8,45 +8,44 @@ import org.junit.rules.ExpectedException;
 public class ValidateEmailTest {
     @Rule
     public ExpectedException exception = ExpectedException.none();
-    private AccountServiceImpl service = new AccountServiceImpl();
 
     @Test
     public void testValidateEmail_empty() throws Exception {
         exception.expect(IncorrectEmailException.class);
-        service.validateEmail("");
+        Validator.validateEmail("");
     }
 
     @Test
     public void testValidateEmail_null() throws Exception {
         exception.expect(IncorrectEmailException.class);
-        service.validateEmail(null);
+        Validator.validateEmail(null);
     }
 
     @Test
     public void testValidateEmail_dog() throws Exception {
-        service.validateEmail("rasulov@gmail.com");
+        Validator.validateEmail("rasulov@gmail.com");
     }
 
     @Test
     public void testValidateEmail_WithoutDog() throws Exception {
         exception.expect(IncorrectEmailException.class);
-        service.validateEmail("rasulov.gmail.com");
+        Validator.validateEmail("rasulov.gmail.com");
     }
 
     @Test
     public void testValidateEmail_nothingAfterDog() throws Exception {
         exception.expect(IncorrectEmailException.class);
-        service.validateEmail("rasulov@");
+        Validator.validateEmail("rasulov@");
     }
 
     @Test
     public void testValidateEmail_nothingBeforeDog() throws Exception {
         exception.expect(IncorrectEmailException.class);
-        service.validateEmail("@gmail.com");
+        Validator.validateEmail("@gmail.com");
     }
     @Test
     public void testValidateEmail_WithoutDomain() throws Exception {
         exception.expect(IncorrectEmailException.class);
-        service.validateEmail("m@m");
+        Validator.validateEmail("m@m");
     }
 }
